5daa9f2a1299a1777759cb44cf642b8e1d50aca6
Tarati <99302555+Taratib@users.noreply.github.com>
2022-06-28 09:21:14 +1200

    Add files via upload

---
+++ 02_AG_Instructions.py
@@ -0,0 +1,30 @@
+def yes_no(question):
+    valid = False
+    while not valid:
+        response = input(question).lower()
+
+        if response == "yes" or response == "y":
+            response = "yes"
+            return response
+
+        elif response == "no" or response == "n":
+            response = "no"
+            return response
+
+        else:
+            print("please answer yes / no")
+            print()
+
+
+def instructions():
+    print("**** Welcome to the Addition Quiz ****")
+    print()
+    print("This is where the rules go.")
+
+    return ""
+
+# Main Routine goes here
+played_before = yes_no("Have you played the game before? ")
+print()
+if played_before == "no":
+    instructions()
